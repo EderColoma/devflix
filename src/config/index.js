e61605eb6dc956f770742ad9014acad1683b462f
Eder Coloma <edermaceno@gmail.com>
2020-09-18 20:08:37 -0300

    Changing data source

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://toeiflix.herokuapp.com';
+  : 'https://developerflix.herokuapp.com';
 
 export default {
   URL_BACKEND,
